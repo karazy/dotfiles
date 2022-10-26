db7b0717e26e8ed354ff5f0942c08867736249c6
Frederik Reifschneider <348098+karazy@users.noreply.github.com>
2022-10-26 08:08:10 +0200

    Update .onboard-doftiles.sh
    added homebrew install and other improvements

--- .onboard-doftiles.sh
+++ .onboard-doftiles.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env bash
 
-# Taken from https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script/18434831#answer-18434831
-case "$OSTYPE" in
-  solaris*) echo "SOLARIS" ;;
-  darwin*)
-    xcode-select --install;; 
-  linux*)
-    sudo apt install -y curl make git;;
-  bsd*)     echo "BSD" ;;
-  msys*)    echo "WINDOWS" ;;
-  *)        echo "unknown: $OSTYPE" ;;
-esac
+OS_NAME=$( uname -s | tr '[:upper:]' '[:lower:]')
+OS_ARCH=$( uname -m )
+
+function installHomebrew {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+}
+
+if ! command -v "git" &> /dev/null; then
+	case "$OS_NAME" in
+	darwin*)
+		xcode-select --install || true
+		;; 
+	linux*)
+		sudo apt install -y curl make git
+		;;
+	*)
+		echo "Unsupported OS: $(uname)"
+		exit 1
+		;;
+	esac
+else
+	echo "Skipping basics installation as they are already installed."
+fi
+
+if ! command -v "brew" &> /dev/null; then
+	case "$OS_ARCH" in
+		aarch64)
+			echo "Skipping homebrew installation as it is unsupported on $OS_ARCH"
+			echo "https://docs.brew.sh/Homebrew-on-Linux#arm-unsupported"
+			;;
+		*)
+			installHomebrew
+			;;
+	esac
+else
+	echo "Skipping homebrew installation as it is already installed."
+fi
 
 
 curl -fsSL https://raw.githubusercontent.com/karazy/dotfiles/main/Makefile | make -f -
